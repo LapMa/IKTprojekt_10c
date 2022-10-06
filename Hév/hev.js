4d622d1733b0f998a90d757ff9343ed2d77ad22a
Lapostyán Martin <lapostyanm@KKSZKI.LOCAL>
2022-10-06 13:11:03 +0200

    Javascript elkészült

---
+++ Hév/hev.js
@@ -0,0 +1,14 @@
+function szamol(){
+    var indulo = document.getElementById("select1").value;
+    var erkezo = document.getElementById("select2").value;
+
+    var eredmeny = indulo-erkezo;
+    if (indulo == erkezo){
+        alert("Azonos megállokat választott ki");
+    }
+    else{
+        alert("Menetidő "+Math.abs(eredmeny)+"perc");
+    }
+    
+
+}
