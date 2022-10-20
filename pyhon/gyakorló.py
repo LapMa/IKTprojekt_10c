a2d31866a4cb118d454f135b20e196762a80a394
Lapostyán Martin <lapostyanm@KKSZKI.LOCAL>
2022-10-20 11:29:57 +0200

    Az első feladat kész

--- pyhon/gyakorló.py
+++ pyhon/gyakorló.py
@@ -1,16 +1,26 @@
 #Feladat1
-""""
+list = []
+
 szam1 = int(input('Kérek egy szmáot: '))
 szam2 = int(input('Kérek egy másik szmáot: '))
 szam3 = int(input('Kérek  egy harmadik számot számot: '))
-if : 
-    print('Ez a legkisseb szám')"""
+
+list.append(szam1)
+list.append(szam2)
+list.append(szam3)
+
+def kisseb():
+    print(min(list))
+
+kisseb()
+
 
 #Feladat2
 
 
 
 #Feladat3
+""""
 x = int(input('Kérek egy dolgozat pontszámot: '))
 if x < 50:
     print("1")
@@ -21,4 +31,8 @@ if 60 <=  x < 70:
 if 70 <= x < 85:
     print("4")
 if x >= 85:
-    print("5") 
+    print("5")"""
+
+#Feladat4
+import numbers
+
