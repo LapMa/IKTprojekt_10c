ddbf977ecc2018b1d91c78ce8f28182146abd03a
Lapostyán Martin <lapostyanm@KKSZKI.LOCAL>
2022-10-20 12:24:29 +0200

    6 feladat kész

--- pyhon/gyakorló.py
+++ pyhon/gyakorló.py
@@ -53,9 +53,29 @@ if x >= 85:
 number = int(input('Kérek egy egész számot: '))
 if number % 3 == 0 or number % 5 == 0 :
     print('Igen, Osztható')
-
 else:
     print('Nem, oszható')"""
 
 #Feladat5
+"""
+szam1 = int(input('Kérek egy szmáot: '))
+szam2 = int(input('Kérek egy másik számot: '))
+szam3 = int(input('Kérek  egy harmadik számot számot: '))
 
+if szam1 + szam2  szam3:
+    print("Első szám + második szám",szam3)
+if szam1 + szam3 = szam2:
+    print("Első szám + harmadik szám",szam3)
+if szam2 + szam3 = szam1:
+    print("Másodi szám + harmadik szám",szam3)"""
+
+#Feladat6
+""""
+szam1 = int(input('Kérek egy számot: '))
+szam2 = int(input('Kérek egy másik számot: '))
+szam3 = int(input('Kérek  egy harmadik számot számot: '))
+
+if szam1 % 2 == 0 and szam2 % 2 == 0 and  szam3 % 2 == 0:
+    print('Igen')
+else:
+    print('Nem ')"""
