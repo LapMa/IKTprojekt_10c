76f7456bbe86063e4d1df7ea778266b0e7bb8fe9
Lapostyán Martin <lapostyanm@KKSZKI.LOCAL>
2022-10-20 11:22:42 +0200

    Harmadik feladattal kezdve

---
+++ pyhon/gyakorló.py
@@ -0,0 +1,24 @@
+#Feladat1
+""""
+szam1 = int(input('Kérek egy szmáot: '))
+szam2 = int(input('Kérek egy másik szmáot: '))
+szam3 = int(input('Kérek  egy harmadik számot számot: '))
+if : 
+    print('Ez a legkisseb szám')"""
+
+#Feladat2
+
+
+
+#Feladat3
+x = int(input('Kérek egy dolgozat pontszámot: '))
+if x < 50:
+    print("1")
+if 50 <= x < 60:
+    print("2") 
+if 60 <=  x < 70:
+    print("3") 
+if 70 <= x < 85:
+    print("4")
+if x >= 85:
+    print("5") 
