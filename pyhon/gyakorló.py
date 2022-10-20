358783262ce2787e44410e777e32fc62f02af5d2
Lapostyán Martin <lapostyanm@KKSZKI.LOCAL>
2022-10-20 11:53:52 +0200

    Feladat 4 kész

--- pyhon/gyakorló.py
+++ pyhon/gyakorló.py
@@ -1,8 +1,9 @@
 #Feladat1
+"""
 list = []
 
-szam1 = int(input('Kérek egy szmáot: '))
-szam2 = int(input('Kérek egy másik szmáot: '))
+szam1 = int(input('Kérek egy számot: '))
+szam2 = int(input('Kérek egy másik számot: '))
 szam3 = int(input('Kérek  egy harmadik számot számot: '))
 
 list.append(szam1)
@@ -12,11 +13,25 @@ list.append(szam3)
 def kisseb():
     print(min(list))
 
-kisseb()
+kisseb()"""
 
 
 #Feladat2
+"""
+list = []
 
+szam1 = int(input('Kérek egy szmáot: '))
+szam2 = int(input('Kérek egy másik számot: '))
+szam3 = int(input('Kérek  egy harmadik számot számot: '))
+
+list.append(szam1)
+list.append(szam2)
+list.append(szam3)
+
+def nagyobb():
+    print(max(list))
+
+nagyobb()"""
 
 
 #Feladat3
@@ -34,5 +49,13 @@ if x >= 85:
     print("5")"""
 
 #Feladat4
-import numbers
+"""
+number = int(input('Kérek egy egész számot: '))
+if number % 3 == 0 or number % 5 == 0 :
+    print('Igen, Osztható')
+
+else:
+    print('Nem, oszható')"""
+
+#Feladat5
 
