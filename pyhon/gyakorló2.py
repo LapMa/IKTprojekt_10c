175d534317965f2164a6ba3999e057768b432afc
Lapostyán Martin <lapostyanm@KKSZKI.LOCAL>
2022-11-10 12:51:45 +0100

    Ciklus első feladat kész

--- pyhon/gyakorló2.py
+++ pyhon/gyakorló2.py
@@ -1,4 +1,5 @@
 import random
+from string import printable
 #1Feladat
 """
 str1 = input("Jó napod van? igen/nem:  ")
@@ -8,7 +9,7 @@ if str1 == "nem":
     print("Szar lehet.")"""
 #1.2Feladat
 """
-elif str1 == "":
+elif str1 != "nem" and str1 != "igen":
     print("Nem értem a válaszodat")"""
 
 #-----------------------------------------
@@ -25,6 +26,23 @@ else:
 
 #3Feladat
 """
-szam2 = int(input("Kéek egy számot"))
-ramdom = random.randint(1.5)
-if """
+szam2 = int(input("Kérek egy számot: "))
+random1 = random.randint(1,5)
+if szam2 == random1:
+    print("A két szám egynelő:")
+if szam2 > random1:
+    print("A szám nagyobb mint a dondolt szám ")
+if szam2 < random1:
+    print("A szám kisebb mint a gondolt szám ")"""
+
+
+#-----------------------------------------
+#1CIKLUS
+
+#1Feladat
+
+for paros in range(10):
+    if paros % 2 ==  0:
+        print(paros)
+
+#2Feladat
