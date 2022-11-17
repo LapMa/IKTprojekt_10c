1806e99e7098d19b624a0f9bf8a3fd53ad6b412c
Lapostyán Martin <lapostyanm@KKSZKI.LOCAL>
2022-11-17 11:17:23 +0100

    Feladat első

---
+++ pyhon/gyakorló3.py
@@ -0,0 +1,12 @@
+"""
+#1Feladat
+szam = 1
+while szam <=20:
+    if szam % 2 == 0:
+        print(szam,"páros")
+        szam += 1
+    elif szam % 2 == 1:
+        print(szam,"páratlan")
+        szam += 1
+"""
+
